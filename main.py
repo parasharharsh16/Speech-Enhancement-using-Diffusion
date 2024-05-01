904c6d749d058a069c4788830a3a94e9fff3623c
harshp <parasharharsh16@gmail.com>
2024-05-01 20:30:31 +0530

    Evalaution of the model and regenerate the audios

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ model = SimpleUnet()
 print("Num params: ", sum(p.numel() for p in model.parameters()))
 train_loader = load_data(dataset_path, "train", batch_size)
 
-model = train(model, lr, train_loader, max_epochs, batch_size, device)
-#save model
-torch.save(model.state_dict(), save_model_path)
+if mode == "train":
+    model = train(model, lr, train_loader, max_epochs, batch_size, device)
+    #save model
+    torch.save(model.state_dict(), save_model_path)
+else:
+    model.load_state_dict(torch.load(save_model_path))
+
+test_loader = load_data(dataset_path, "test", batch_size=1)
+predictions,inputwave, test_loss = evaluate(model, test_loader, device)
+print(f"Test Loss: {test_loss}")
+#save samples
+save_samples(predictions,inputwave)
+
+#save sample
+#convert_to_audio(predictions,hop_length=512)
+
--- utills.py
+++ utills.py
@@ -1,12 +1,14 @@
 from torch.utils.data import dataloader
 from dataprep import *
+from model import *
 from torch import nn
 import math
 from tqdm import tqdm
 from torch.optim import Adam
 import torch.nn.functional as F
 import wandb
-
+import matplotlib.pyplot as plt
+import soundfile as sf
 #function to load the data to dataloder
 def load_data(dataset_path, mode, batch_size):
     dataset = VCTKDataset(dataset_path, mode, sample_size=sample_size, train_ratio=train_ratio)
@@ -23,113 +25,19 @@ def compute_mse(spectrogram1, spectrogram2):
     
     return mse
 
-
-
-class Block(nn.Module):
-    def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
-        super().__init__()
-        self.time_mlp =  nn.Linear(time_emb_dim, out_ch)
-        if up:
-            self.conv1 = nn.Conv2d(2*in_ch, out_ch, 3, padding=1)
-            self.transform = nn.ConvTranspose2d(out_ch, out_ch, 4, 2, 1)
-        else:
-            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
-            self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1)
-        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-        self.bnorm1 = nn.BatchNorm2d(out_ch)
-        self.bnorm2 = nn.BatchNorm2d(out_ch)
-        self.relu  = nn.ReLU()
-        
-    def forward(self, x, t, ):
-        # First Conv
-        h = self.bnorm1(self.relu(self.conv1(x)))
-        # Time embedding
-        time_emb = self.relu(self.time_mlp(t))
-        # Extend last 2 dimensions
-        time_emb = time_emb[(..., ) + (None, ) * 2]
-        # Add time channel
-        h = h + time_emb
-        # Second Conv
-        h = self.bnorm2(self.relu(self.conv2(h)))
-        # Down or Upsample
-        return self.transform(h)
-
-
-class SinusoidalPositionEmbeddings(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.dim = dim
-
-    def forward(self, time):
-        device = time.device
-        half_dim = self.dim // 2
-        embeddings = math.log(10000) / (half_dim - 1)
-        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
-        embeddings = time[:, None] * embeddings[None, :]
-        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
-        # TODO: Double check the ordering here
-        return embeddings
-
-
-class SimpleUnet(nn.Module):
-    """
-    A simplified variant of the Unet architecture.
-    """
-    def __init__(self):
-        super().__init__()
-        image_channels = 1
-        down_channels = (64, 128, 256, 512, 1024)
-        up_channels = (1024, 512, 256, 128, 64)
-        out_dim = 1 
-        time_emb_dim = 32
-
-        # Time embedding
-        self.time_mlp = nn.Sequential(
-                SinusoidalPositionEmbeddings(time_emb_dim),
-                nn.Linear(time_emb_dim, time_emb_dim),
-                nn.ReLU()
-            )
-        
-        # Initial projection
-        self.conv0 = nn.Conv2d(image_channels, down_channels[0], 3, padding=1)
-
-        # Downsample
-        self.downs = nn.ModuleList([Block(down_channels[i], down_channels[i+1], \
-                                    time_emb_dim) \
-                    for i in range(len(down_channels)-1)])
-        # Upsample
-        self.ups = nn.ModuleList([Block(up_channels[i], up_channels[i+1], \
-                                        time_emb_dim, up=True) \
-                    for i in range(len(up_channels)-1)])
-        
-        # Edit: Corrected a bug found by Jakub C (see YouTube comment)
-        self.output = nn.Conv2d(up_channels[-1], out_dim, 1)
-
-    def forward(self, x, timestep):
-        # Embedd time
-        t = self.time_mlp(timestep)
-        # Initial conv
-        x = self.conv0(x)
-        # Unet
-        residual_inputs = []
-        for down in self.downs:
-            x = down(x, t)
-            residual_inputs.append(x)
-        for up in self.ups:
-            residual_x = residual_inputs.pop()
-            # Add residual x as additional channels
-            x = torch.cat((x, residual_x), dim=1)           
-            x = up(x, t)
-        return self.output(x)
-
-def get_loss(model, noise,clean, t, device):
-    clean_spectogran, noisy = forwad_diff(clean, noise,t,device)
-    clean_spectogran = clean_spectogran.unsqueeze(1)
-    noisy = noisy.unsqueeze(1)
+def get_loss(model, noise, clean, t, device):
+    clean_stft, _ = get_stft(clean)
+    noise_stft, _ = get_stft(noise)
+    #clean, noisy = forwad_diff(clean_stft, noise_stft, t, device)
+    clean_stft = torch.tensor(clean_stft).to(device)
+    noisy_stft = torch.tensor(noise_stft).to(device)
+    clean = clean_stft.unsqueeze(1)
+    noisy = noisy_stft.unsqueeze(1)
+    
     t= t.to(device)
     pred = model(noisy, t)
     
-    return F.l1_loss(clean_spectogran, pred)
+    return F.l1_loss(clean, pred),pred
 
 #traing loop
 def train(model,learning_rate,train_loader,epochs,batch_size,device):
@@ -159,9 +67,78 @@ def train(model,learning_rate,train_loader,epochs,batch_size,device):
             batch_size = clean.shape[0]
             optimizer.zero_grad()
             t = torch.randint(0, 10, (batch_size,), device='cpu').long()
-            loss = get_loss(model, noise,clean, t,device)
+            loss,_ = get_loss(model, noise,clean, t,device)
             loss.backward()
             optimizer.step()
             pbar.set_postfix({'loss': loss.item()})
         wandb.log({"Train loss": loss.item()})
     return model
+
+#evaluate model
+def evaluate(model, test_loader, device):
+    model.to(device)
+    model.eval()
+    total_loss = 0
+    predictions = []
+    inputwave = []
+    eval_pbar = tqdm(test_loader, desc="Evaluating")
+    count = 1
+    with torch.no_grad():
+        for batch in eval_pbar:
+            clean, noise = batch
+            batch_size = clean.shape[0]
+            t = torch.randint(0, 10, (batch_size,), device='cpu').long()
+            loss,pred = get_loss(model, noise,clean, t,device)
+            total_loss += loss.item()
+            predictions.append(pred)
+            inputwave.append(noise)
+    return predictions,inputwave, total_loss/len(test_loader)
+
+#visualize the list of specogram
+def visualize_spectogram(spectograms):
+    spectrogram_shape = spectograms[0].shape
+    # Calculate the figure size based on the spectrogram size
+
+    fig, axs = plt.subplots(1, len(spectograms), figsize=(20, 10))
+    for i, spectrogram in enumerate(spectograms):
+        spectrogram = spectrogram.squeeze(0).squeeze(0)
+        spectrogram = spectrogram.detach().cpu().numpy()
+        axs[i].imshow(spectrogram, aspect='auto', origin='lower')
+        axs[i].set_title(f'Spectrogram {i+1}')
+    plt.savefig('output/spectogram.png')
+    plt.show()
+
+
+
+def get_stft(waveform):
+    if not isinstance(waveform, np.ndarray):
+        waveform = np.array(waveform)
+    stft = librosa.stft(waveform, n_fft=n_fft, hop_length=hop_length)
+    magnitude, phase = librosa.magphase(stft)
+    magnitude = magnitude[:,:1024, :64]
+    phase = phase[:,:1024, :64]
+    magnitude = np.pad(magnitude, ((0, 0), (0, 0), (0, 1)))
+    phase = np.pad(phase, ((0, 0), (0, 0), (0, 1)))
+    return magnitude, phase
+
+#Regerate the wave from the predicted stft
+def invert_stft_to_audio(generated, input_wave,n_fft = 2048, hop_length = 512):
+    if not isinstance(input_wave, np.ndarray):
+        input_wave = np.array(input_wave)
+    if not isinstance(generated, np.ndarray):
+        generated = np.array(generated)
+    _,phase = get_stft(input_wave)
+
+    waveform_reg = librosa.istft(generated * phase, hop_length=hop_length)
+    return waveform_reg
+
+def save_samples(predictions,inputwave):
+    count = 0
+    for i, pred in enumerate(predictions):
+        pred = pred.squeeze(0).squeeze(0)
+        pred = pred.detach().cpu().numpy()
+        waveform_reg = invert_stft_to_audio(pred,inputwave[i].detach().numpy(), hop_length=512)
+        sf.write(f'output/sample_{i}.wav', waveform_reg.squeeze(0), 16000)
+        if count == 10:
+            break
+        count += 1
